63ac3505cb3af199aa3f30f253c9f0c72fa87a91
Victor <victor.alencar.oliveira@outlook.com>
2025-07-31 14:46:09 -0300

    Adicionando javascript simples e ajustando o html e css

---
+++ portifolio/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Adiciona rolagem suave para links de âncora
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
